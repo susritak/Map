61dabac835aa42b1fa774555fc5541070a8cd98c
Susrita Khadka <mithi@Susritas-MBP.home>
2024-04-26 15:55:41 +0200

    Add production-ready build files

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
+function MapApplication() {
+    return <>
+      <header><h1>Map Application</h1></header>
+      <nav>Actions</nav>
+      <main> Here is the map</main>
+    </>;
+}
 root.render(<h1>Mapping Application</h1>)
