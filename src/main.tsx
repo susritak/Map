5ab18a2f49e833afba4f3f8e9043d53b609b74e0
Susrita Khadka <mithi@Susritas-MBP.home>
2024-04-26 15:12:46 +0200

    Update project configuration and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello React</h1>)
+const root = ReactDOM.createRoot(document.getElementById("root")!);
