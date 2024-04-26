18f1a5f663f8f584c71f624b71226a89ce3b160b
Susrita Khadka <mithi@Susritas-MBP.home>
2024-04-26 15:53:31 +0200

    Add production-ready build files

--- src/components/application/mapApplication.tsx
+++ src/components/application/mapApplication.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-export function MapApplication() {
-    return <>
+function MapApplication() {
+  return (
+    <>
       <header>
         <h1>Map Application</h1>
       </header>
       <nav>Actions</nav>
-      <main> Here is the map</main>
-    </>;
+      <main>Here is the map</main>
+    </>
+  );
 }
+
+export default MapApplication;
